46a24678c26a5773abb52d92fc5748aeede5bbe0
Robert Hsu <roberthsu2003@gmail.com>
2018-11-24 16:02:58 +0800

    Update content1.py

--- content/content1.py
+++ content/content1.py
@@ -6,3 +6,13 @@ print(a);
 for i in  range(1, 10):
     print(i);
     
+a = 123;
+b = 12.34;
+c = "Hello";
+d = 'Hello';
+e = True;
+
+x = input("Enter Value:");
+print(type(x));
+print("輸入值為:",x);
+    
