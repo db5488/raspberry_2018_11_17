a4134bd12a113a77eaf890506f724704ea05642f
robethsu <roberthsu2003@gmail.com>
2018-12-01 14:23:09 +0800

    modify

--- python/button.py
+++ python/button.py
@@ -1,10 +1,14 @@
-from gpiozero import Button
+from gpiozero import Button, LED
+
 
 button = Button(17)
+led = LED(27);
 while True:
     if button.is_pressed :
         print('按下');
+        led.on();
     else:
         print('放開');
+        led.off();
 
 
