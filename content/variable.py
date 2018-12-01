745c1fa76f3b4d54eb7ab44d4dedcf38b8dee558
robethsu <roberthsu2003@gmail.com>
2018-12-01 10:15:12 +0800

    modify

--- content/variable.py
+++ content/variable.py
@@ -5,3 +5,27 @@ print(type(b));
 print(type(58));
 print(type(99.9));
 print(type('abc'));
+print(type(9/5));
+print(type(9//5));
+a = 95
+a += 3
+print("a=",a);
+
+print((2 + 3) * 4);
+print(10);
+print(0b10)
+print(0o10)
+print(0x10)
+
+#type conversion
+print(int(True));
+print(int(False));
+print(int(98.6));
+print(int(1.0e4));
+#print(int('99 bottles of beer on the wall'));
+print(type(4 + 7));
+print(True + 2);
+googol = 10 **1000
+print(type(googol))
+print(float('99'));
+print("Nay,'said the naysayer");
