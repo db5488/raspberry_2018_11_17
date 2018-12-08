5ec5dcfeefd12faa32eccab97370a1cc77ef3fe4
robethsu <roberthsu2003@gmail.com>
2018-12-08 15:21:53 +0800

    modify

--- Firebase_GPIO_tkinter/0_2button_seven_segment_display/button_seven_segment.py
+++ Firebase_GPIO_tkinter/0_2button_seven_segment_display/button_seven_segment.py
@@ -1,7 +1,11 @@
 import RPi.GPIO as GPIO
 import time
 
+
+
 GPIO.setwarnings(False);
 GPIO.setmode(GPIO.BCM);
 GPIO.setup(17,GPIO.OUT);
 GPIO.output(17, GPIO.HIGH);
+time.sleep(10);
+GPIO.cleanup();
