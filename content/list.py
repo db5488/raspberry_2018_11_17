49dd94071874df1a2fa567840f714a3ac9117ff3
robethsu <roberthsu2003@gmail.com>
2018-12-08 09:59:17 +0800

    modify

--- content/list.py
+++ content/list.py
@@ -8,3 +8,23 @@ print(first_names)
 
 another_empty_list = list();
 print(another_empty_list);
+
+print(list('cat'));
+
+a_tuple = ('ready', 'fire', 'aim');
+print(list(a_tuple));
+birthday = '1/6/1952';
+print(birthday.split('/'));
+splitme = 'a/b//c/d///e';
+print(splitme.split('/'));
+
+marxes = ['Groucho', 'Chico', 'Harpo'];
+print(marxes[0]);
+print(marxes[1]);
+print(marxes[2]);
+#print(marxes[3]);
+
+print(marxes[-1]);
+
+print(marxes[-2]);
+print(marxes[-3]);
