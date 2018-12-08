1dc28cbff42488d02776402c40ac66e32d7e1dbd
robethsu <roberthsu2003@gmail.com>
2018-12-08 11:56:41 +0800

    modify

--- Firebase_GPIO_tkinter/0_1button_RGBLED/button_RGBLED.py
+++ Firebase_GPIO_tkinter/0_1button_RGBLED/button_RGBLED.py
@@ -4,3 +4,20 @@ from time import sleep
 led = RGBLED(red=17, green=27, blue=22);
 led.blue = 1;
 sleep(1);
+
+led.color = (1, 0, 0); #full green
+sleep(1);
+led.color = (1, 0, 1); # magenta
+sleep(1);
+led.color = (1, 1, 0); # yellow
+sleep(1);
+led.color = (0, 1, 1); #cyan
+sleep(1);
+led.color = (1, 1, 1); #white
+sleep(1);
+led.color = (0, 0, 0);
+sleep(1);
+
+for n in range(100):
+    led.blue = n/100;
+    sleep(0.1);
