93ae7df729c4fdbd2fece7e7e922360719b4760a
robethsu <roberthsu2003@gmail.com>
2018-12-08 13:55:32 +0800

    modify

--- Firebase_GPIO_tkinter/0_1button_RGBLED/button_RGBLED.py
+++ Firebase_GPIO_tkinter/0_1button_RGBLED/button_RGBLED.py
@@ -1,23 +1,17 @@
-from gpiozero import RGBLED
+from gpiozero import RGBLED,Button
 from time import sleep
+from signal import pause
+from random import randint
 
+def user_press():
+    r = randint(0,100) / 100;
+    g = randint(0,100) / 100;
+    b = randint(0,100) / 100;
+    print("R={},G={},B={}".format(r,g,b));
+    led.color = (r, g, b);
+
+button = Button(18);
 led = RGBLED(red=17, green=27, blue=22);
-led.blue = 1;
-sleep(1);
 
-led.color = (1, 0, 0); #full green
-sleep(1);
-led.color = (1, 0, 1); # magenta
-sleep(1);
-led.color = (1, 1, 0); # yellow
-sleep(1);
-led.color = (0, 1, 1); #cyan
-sleep(1);
-led.color = (1, 1, 1); #white
-sleep(1);
-led.color = (0, 0, 0);
-sleep(1);
-
-for n in range(100):
-    led.blue = n/100;
-    sleep(0.1);
+button.when_pressed = user_press;
+pause();
