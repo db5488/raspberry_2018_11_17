cf8c7e1afe7fdf6c9992b351f1dbac0b5704447f
robethsu <roberthsu2003@gmail.com>
2018-12-15 14:46:33 +0800

    modify

--- Firebase_GPIO_tkinter/1LEDControl/c1_LEDControl.py
+++ Firebase_GPIO_tkinter/1LEDControl/c1_LEDControl.py
@@ -2,7 +2,9 @@ from tkinter import *
 
 def appInterface(window):
     frame = Frame(window,borderwidth=1,relief=GROOVE);
-    Label(frame,text="LED Controler").pack();
+    Label(frame,text="LED Controler",font=("Helvetica", 24),anchor=W).pack(fill=X);
+    Button(frame,text="open").pack(side=LEFT);
+    Button(frame,text="close").pack(side=LEFT);
     frame.pack(padx=10,pady=10,fill=X);
 
 
@@ -11,7 +13,7 @@ if __name__ == '__main__':
     app.title("LEDControl");
     app.geometry("500x500");
     app.option_add("*Button.Background","#007A9B");
-    app.option_add("*Button.Foreground","#white");
+    app.option_add("*Button.Foreground","white");
     appInterface(window=app);
     app.mainloop();
 
