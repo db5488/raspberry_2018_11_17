a552e356366ad32870656e741663dc9e07b9308f
robethsu <roberthsu2003@gmail.com>
2018-12-15 13:49:55 +0800

    modify

--- Firebase_GPIO_tkinter/1LEDControl/c1_LEDControl.py
+++ Firebase_GPIO_tkinter/1LEDControl/c1_LEDControl.py
@@ -1,7 +1,15 @@
 from tkinter import *
 
+def appInterface(window):
+    print(window);
+
 
 if __name__ == '__main__':
-    root = Tk();
-    root.title("LEDControl");
-    root.mainloop();
+    app = Tk();
+    app.title("LEDControl");
+    app.geometry("500x500");
+    app.option_add("*Button.Background","#007A9B");
+    app.option_add("*Button.Foreground","#white");
+    appInterface(window=app);
+    app.mainloop();
+
