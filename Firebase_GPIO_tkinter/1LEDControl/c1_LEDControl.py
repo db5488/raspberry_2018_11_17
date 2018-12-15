72c93238b77cfd9d4431c83f917ccfa85fb3bc88
robethsu <roberthsu2003@gmail.com>
2018-12-15 15:09:36 +0800

    modify

--- Firebase_GPIO_tkinter/1LEDControl/c1_LEDControl.py
+++ Firebase_GPIO_tkinter/1LEDControl/c1_LEDControl.py
@@ -1,10 +1,34 @@
 from tkinter import *
 
+def ledClose():
+    print("ledClose");
+    
+def ledOpen():
+    print("ledOpen");
+
 def appInterface(window):
     frame = Frame(window,borderwidth=1,relief=GROOVE);
-    Label(frame,text="LED Controler",font=("Helvetica", 20),anchor=W).pack(fill=X);
-    Button(frame,text="open",pady=50,font=("Helvetica", 20)).pack(side=LEFT,fill=X,expand=True,padx=10,pady=20);
-    Button(frame,text="close",pady=50,font=("Helvetica", 20)).pack(side=LEFT,fill=X,expand=True,padx=10,pady=20);
+    Label(frame,
+          text="LED Controler",
+          font=("Helvetica", 20)
+          ,anchor=W).pack(fill=X);
+    
+    Button(frame,text="open",
+           pady=50,
+           font=("Helvetica", 20),
+           command=ledOpen).pack(side=LEFT,
+                                        fill=X,
+                                        expand=True,
+                                        padx=10,
+                                        pady=20);
+    
+    Button(frame,text="close"
+           ,pady=50,           
+           font=("Helvetica", 20),command=ledClose).pack(side=LEFT,
+                                        fill=X,
+                                        expand=True,
+                                        padx=10,
+                                        pady=20);
     frame.pack(padx=10,pady=10,fill=X);
 
 
