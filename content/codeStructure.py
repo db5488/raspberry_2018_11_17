9ed2c22388c50232c4dab6ea74734c882ee1b70d
robethsu <roberthsu2003@gmail.com>
2018-12-15 10:18:35 +0800

    modify

---
+++ content/codeStructure.py
@@ -0,0 +1,15 @@
+alphabet = 'abcdefg' + \
+'hijklmnop' + \
+'efg' + \
+'wxyz';
+print(alphabet);
+
+color ="puce";
+if color == "red":
+    print("tomato");
+elif color == "green":
+    print("pepper");
+elif color == "bee purple":
+    print("bees");
+else:
+    print(color);
