a00b35db1fd7bc247d92f75e1b7d4e01c354d05b
robethsu <roberthsu2003@gmail.com>
2018-12-15 10:00:19 +0800

    modify

--- content/dictionary.py
+++ content/dictionary.py
@@ -23,3 +23,26 @@ pythons['Gilliam'] = 'Gerry';
 print(pythons);
 pythons['Gilliam'] = 'Terry';
 print(pythons);
+
+others = {'Marx': 'Groucho', 'Howard':'Moe'};
+pythons.update(others);
+print(pythons);
+del pythons['Marx'];
+print(pythons);
+#pythons.clear();
+#print(pythons);
+print('Chapman1' in pythons);
+#print(pythons['Marx']);
+if 'Marx' in pythons:
+    pythons['Marx'];
+else:
+    print('no key')
+
+print(pythons.get('Marx', 'Not a python'));
+
+print(list(pythons.keys()));
+print(list(pythons.values()));
+print(list(pythons.items()));
+
+newPythons = pythons.copy();
+print(newPythons);
